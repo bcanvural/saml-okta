f41ab1a9ecd362c746556b2aab128862f7010fab
Baris Can Vural <bcanvural@gmail.com>
2019-05-10 13:37:47 +0200

    Inital commit

---
+++ src/main/java/com/github/bcanvural/samlokta/SamlOktaApplication.java
@@ -0,0 +1,13 @@
+package com.github.bcanvural.samlokta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlOktaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlOktaApplication.class, args);
+	}
+
+}
